a38c4236017d72a532d15ca4731b68f444a89c36
songweizhi <songwz03@gmail.com>
2020-09-01 11:59:50 +1000

    -

--- GTDB-Tk_r95_2020-09-01.sh
+++ GTDB-Tk_r95_2020-09-01.sh
@@ -12,12 +12,14 @@ pip3 install --upgrade gtdbtk
 
 
 # download db files (no need to do this on Katana)
-# cd /srv/scratch/z5039045/DB/gtdbtk/release89
-# wget https://data.ace.uq.edu.au/public/gtdb/data/releases/release89/89.0/gtdbtk_r89_data.tar.gz
+# cd /srv/scratch/z5039045/DB/gtdbtk/release95
+# wget https://data.ace.uq.edu.au/public/gtdb/data/releases/release89/89.0/gtdbtk_r95_data.tar.gz
 # tar xvzf gtdbtk_r89_data.tar.gz
 
 
-####################################### run GTDB-TK ######################################
+########################## Taxonomic classification with GTDB-TK #########################
+
+# please request as much memory as you can for the classify_wf module, e.g. > 160 Gbp
 
 module load python/3.7.3
 source ~/mypython3env/bin/activate
@@ -41,9 +43,23 @@ gtdbtk classify_wf --cpus 6 --pplacer_cpus 1 --genome_dir NS_37bins --extension 
 # <prefix>.ar122.summary.tsv
 
 
-################################# get Tree with GTDB-TK ##################################
+################################# Infer tree with GTDB-TK ################################
 
-gtdbtk identify
-gtdbtk align
-gtdbtk infer
+module load python/3.7.3
+source ~/mypython3env/bin/activate
+module load perl/5.28.0
+module load prodigal/2.6.3
+module load pplacer/1.1.alpha19
+module load hmmer/3.3
+module load fasttree/2.1.11
+module load gcc/6.2.0
+module load gsl/2.6
+module load fastani/1.3
+module load R/3.5.3
+export GTDBTK_DATA_PATH=/data/bio/gtdbtk/release95
+
+cd /srv/scratch/z5039045/MetaCHIP_demo
+gtdbtk identify --genome_dir NS_37bins -x fasta --out_dir NS_37bins_GTDB_r95_tree --cpus 6
+gtdbtk align --identify_dir NS_37bins_GTDB_r95_tree --out_dir NS_37bins_GTDB_r95_tree --cpus 6
+gtdbtk infer --msa_file NS_37bins_GTDB_r95_tree/gtdbtk.bac120.user_msa.fasta --out_dir NS_37bins_GTDB_r95_tree --cpus 6
 
